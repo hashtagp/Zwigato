dfca7d457612a27709e92a98137331931244d66d
hashtagp <hashtag.p27@gmail.com>
2025-01-03 21:17:49 +0530

    removed temp edits

--- Backend/models/foodModel.js
+++ Backend/models/foodModel.js
@@ -9,6 +9,6 @@ const foodSchema= new mongoose.Schema({
 })
 
 //or operation to check if food model already present
-const foodModel =mongoose.models.food || mongoose.model("Product",foodSchema);
+const foodModel =mongoose.models.food || mongoose.model("food",foodSchema);
 
 export default foodModel;
